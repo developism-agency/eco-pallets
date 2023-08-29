b748bb3512261b0da1c02e37eb4fd3d379bb12b2
Bohdan Tomchuk <bogdan.tomchuck@gmail.com>
2023-08-29 11:50:01 +0300

    add images

--- static/js/main.js
+++ static/js/main.js
@@ -73,17 +73,12 @@ scrollDownBtn.addEventListener('click', () => {
 })
 
 //embed map address
-const addresses = {
-  firstAddress: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2564.9143067351333!2d24.277662982750268!3d49.99421604332794!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x473ace56df37893f%3A0xa79093f4a54544f5!2sSchool%20of%20Velyke%20Kolodno!5e0!3m2!1suk!2sua!4v1693070158395!5m2!1suk!2sua",
-  secondAddress: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2743.7518396693945!2d15.669558776687664!3d46.55259566016454!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x476f771770cbf365%3A0x2afd82719b7fc5aa!2zR3VuZHVsacSNZXZhIHVsaWNhIDEyLCAyMDAwIE1hcmlib3IsINCh0LvQvtCy0LXQvdGW0Y8!5e0!3m2!1suk!2sua!4v1693070311213!5m2!1suk!2sua",
-}
-
 const map = document.querySelector('.map__map iframe')
 
 document.querySelectorAll('.btn-link').forEach(btn => {
   btn.addEventListener('click', () => {
-    if (map.src === addresses[btn.dataset.address]) return
-    else map.src = addresses[btn.dataset.address]
+    if (map.src === btn.dataset.address) return
+    else map.src = btn.dataset.address
     
   })
 })
